0f898642ea91cf3cad5a686aec5e97e453640df0
vagrant <you@example.comwritercassiejeon@gmail.com>
2015-10-14 21:18:45 +0000

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Accountable::Application.routes.draw do
+
+  root 'goals#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
