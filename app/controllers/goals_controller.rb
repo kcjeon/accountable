26d708c35a5eb3b1b9536d695a9d8d1d5c36e9bb
vagrant <you@example.comwritercassiejeon@gmail.com>
2015-10-20 13:35:16 +0000

    ready

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -6,4 +6,15 @@ class GoalsController < ApplicationController
 	def new
 		@goal = Goal.new
 	end
+
+	def create
+		Goal.create(goal_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def goal_params
+		params.require(:goal).permit(:desire, :author)
+	end
 end
